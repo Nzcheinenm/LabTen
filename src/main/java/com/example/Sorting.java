c4f3ff9fddc9e876b001aa37fc9c6e8fcdcdc01d
Dmitry <koliasam@yandex.ru>
2019-11-18 10:05:14 +0300

    Second commit to adding tests

--- src/main/java/com/example/Sorting.java
+++ src/main/java/com/example/Sorting.java
@@ -3,16 +3,34 @@ package com.example;
 import java.util.Arrays;
 
 final class Sorting {
-        private Sorting() { }
 
         /**
-         * Сообщение для защиты.
+         * Сообщение для защиты от пустоты.
          */
         static final String MESSAGE = "Массив пуст "
             + "или из одного элемента";
+        /**
+         * Сообщение для защиты
+         * одинаковых элементов.
+         */
+        static final String MESSAGE_DOUBLE = "Массив одинаковых "
+            + "элементов";
+        /**
+         * Сообщение для защиты размера.
+         */
+        static final String MESSAGE_EXIT = "Массив превышает "
+            + "10 элементов";
+
+        private Sorting() {
+        }
 
         static String combineSort(final int[] arr) {
                 int lenArr = arr.length;
+
+                if (lenArr > 10) {
+                        return MESSAGE_EXIT;
+                }
+
                 if (lenArr <= 1) {
                         return MESSAGE;
                 } else {
@@ -29,6 +47,18 @@ final class Sorting {
                                         }
                                 }
                         }
+
+                        int countI = 0;
+                        for (int i = 0; i <= arr.length - 1; i++) {
+                                if (arr[0] == arr[i]) {
+                                        countI = countI + 1;
+                                }
+                        }
+
+                        if (countI == arr.length) {
+                                return MESSAGE_DOUBLE;
+                        }
+
                         return Arrays.toString(arr);
                 }
         }
--- src/test/java/com/example/SortTest.java
+++ src/test/java/com/example/SortTest.java
@@ -4,9 +4,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SortTest {
-        private final int[] a = {1, 2, 3, 4, 10, 5, 9, 8, 7}; // Массив
+
+        private final int[] a = {5, 2, 3, 4, 10, 1, 9, 8, 7}; // Массив
         private final int[] sortToA = {1, 2, 3, 4, 5, 7, 8, 9, 10}; // Отсортированный
         private final int[] aNull = {}; // Пустой массив
+        private final int[] aDouble = {1, 1, 1, 1, 1}; // Массив одного элемента
+        private final int[] aSize = {1, 1, 1, 1, 2, 1, 3, 4, 5 , 9, 11}; // Массив одного элемента
 
         @Test
         public void testingInSort() {
@@ -29,5 +32,17 @@ public class SortTest {
                 long t3 = t2 - t1;
                 Assert.assertFalse("Время выполнения больше чем ожидается",
                     (t3 <= a.length * Math.log(a.length)));
+        }
+
+        @Test
+        public void testingInDouble() {
+                Assert.assertEquals("Не обработана возможность "
+                    + "массива одинаковых чисел", Sorting.combineSort(aDouble), Sorting.MESSAGE_DOUBLE);
+        }
+
+        @Test
+        public void testingInSize() {
+                Assert.assertEquals("Не обработан массив "
+                    + "превышающий 10 элементов", Sorting.combineSort(aSize), Sorting.MESSAGE_EXIT);
         }
 }
